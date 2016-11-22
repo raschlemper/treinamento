bd4d657ca1a5b2cb81837d79f9a08b1d4b82c040
rafael.schlemper <rafael.schlemper@gennera.com.br>
2016-11-22 16:26:14 -0200

    ajuste rota

--- client/js/service/authService.js
+++ client/js/service/authService.js
@@ -3,17 +3,19 @@
 app.factory('AuthService', ['$http', '$localStorage', 'PromiseService', 
     function($http, $localStorage, PromiseService) {
 
+    var adminUrl = 'https://ras-administration.herokuapp.com';
+
     return {
 
         login: function(email, password) {
             return PromiseService.execute(
-                    $http.post("http://localhost:3000/auth/local", 
+                    $http.post(adminUrl + "/auth/local", 
                         { email: email, password: password }));
         },
 
         isAuthenticated: function() {
             return PromiseService.execute(
-                    $http.get("http://localhost:3000/auth/authenticated"));
+                    $http.get(adminUrl + "/auth/authenticated"));
         },
 
         createToken: function(token) {
