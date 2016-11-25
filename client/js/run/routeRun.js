de87e33dd1d2dd7e25eefa993200b06a4f605f2f
Raschlemper <raschlemper@gmail.com>
2016-11-24 23:22:03 -0200

    ajustes

--- client/js/run/routeRun.js
+++ client/js/run/routeRun.js
@@ -1,16 +1,16 @@
 app.run(['$rootScope', '$state', function ($rootScope, $state) {    
 
-    $rootScope.goTo = function(state, params) {
-        $state.go(state, params);
+    $rootScope.goTo = function(state, params, options) {
+        $state.go(state, params, options);
     };
 
     $rootScope.goToLogin = function() {
-    		event.preventDefault();
+    	  event.preventDefault();
         $rootScope.goTo('auth.login', null, { reload: true });
     };
 
     $rootScope.goToIndex = function() {
-    		event.preventDefault();
+    	  event.preventDefault();
         $rootScope.goTo('app.java.start', null, { reload: true });
     };
 
