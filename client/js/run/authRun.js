4627a99bced45009a68742ef6466ae2721b40f2e
Raschlemper <raschlemper@gmail.com>
2016-11-24 23:00:17 -0200

    ajustes

--- client/js/run/authRun.js
+++ client/js/run/authRun.js
@@ -2,10 +2,25 @@ app.run(['$rootScope', 'AuthService', 'RouteService',
     function ($rootScope, AuthService, RouteService) {    
 
     $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+        console.log(toState, toParams);
+        setToken(toState, toParams);
+        hasToken(toState, toParams);
+        authenticated(toState, toParams);
+    });
+
+    var setToken = function(toState, toParams) {
         if(toParams && toParams.token) { 
-            console.log(toState, toParams);
             AuthService.createToken(toParams.token);
         }
+    };
+
+    var hasToken = function(toState, toParams) {
+        if(toState && toState.name = "app.token") { 
+            $rootScope.goToIndex(); 
+        }
+    };
+
+    var authenticated = function(toState, toParams) {
         if(!RouteService.isPublic(toState.name)) {
             AuthService.isAuthenticated(AuthService.getToken())
                 .then(function(data) {
@@ -16,7 +31,7 @@ app.run(['$rootScope', 'AuthService', 'RouteService',
                     event.preventDefault(); 
                     $rootScope.goToLogin();                   
                 });
-        }
-    });
+        }        
+    }
 
 }]);
