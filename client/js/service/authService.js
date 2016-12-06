2ea85777a3c9749b8fe58fb1888f72d160de2857
Raschlemper <raschlemper@gmail.com>
2016-12-06 00:10:52 -0200

    merge

--- client/js/service/authService.js
+++ client/js/service/authService.js
@@ -4,39 +4,30 @@ app.factory('AuthService', ['$http', '$localStorage', 'PromiseService',
     function($http, $localStorage, PromiseService) {
 
     var adminUrl = 'https://ras-administration.herokuapp.com';
+    var system = '5836d5d41af5c70012d6d58e';
 
     return {
 
-        login: function(email, password) {
-            return PromiseService.execute(
-                    $http.post(adminUrl + "/auth/login/local/5836d5d41af5c70012d6d58e", 
-                        { email: email, password: password }));
-        },
-
-        loginGoogle: function() {
-            return PromiseService.execute(
-                    $http.get(adminUrl + "/auth/google"));
-        },
-
         isAuthenticated: function() {
             return PromiseService.execute(
-                    $http.get(adminUrl + "/auth/authenticated"));
+                    $http.get(adminUrl + "/auth/authenticated/" + system));
+        },
+
+        getSystem: function() {
+            return system;
+        },
+
+        getUser: function() {
+            return PromiseService.execute(
+                    $http.get(adminUrl + "/auth/token/user"));
         },
 
         createToken: function(token) {
-        	$localStorage.token = token;
+            $localStorage.token = token;
         },
 
-		getToken: function() {
-        	return $localStorage.token;
-        },
-
-        createUser: function(user) {
-        	$localStorage.user = user;
-        },
-
-		getUser: function() {
-        	return $localStorage.user;
+        getToken: function() {
+            return $localStorage.token;
         }
 
     }
