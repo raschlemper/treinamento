f7612b7a4a7702a87cc6a4d5c1443309c0eabdea
rafael.schlemper <rafael.schlemper@gennera.com.br>
2016-12-08 11:31:10 -0200

    ajustes

--- client/js/controller/authController.js
+++ client/js/controller/authController.js
@@ -11,7 +11,7 @@ app.controller('AuthController', ['$rootScope', '$scope', '$state', '$stateParam
 	};
 
 	var getUrl = function(strategy) {
-		var url  = AuthService.getUrlAdmin() + '/';
+		var url  = AuthService.getUrlAdmin() + '/auth/';
 		    url += strategy + '/';
 		    url += AuthService.getSystem();
 		    url += '?target=' + getTarget();
